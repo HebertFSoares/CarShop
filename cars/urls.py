7edcf8177f53f5bde1edf460c5faaff60c958eab
Hebert Soares <hebertsoaresof@gmail.com>
2023-04-09 23:12:05 -0300

    Consultas com icontains e order_by

--- cars/urls.py
+++ cars/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('my_cars/', views.cars_view, name="cars"),
+    path('my_cars/', views.cars_view, name="cars_list"),
 ]
