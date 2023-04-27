0c639b9f10cd3d36883bd55b4e7c8046227c93c4
Hebert Soares <hebertsoaresof@gmail.com>
2023-04-27 17:18:33 -0300

    finalização das views de cadastro

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+
 
 def register(request):
-    use_form = UserCreationForm()
-    return render(request, 'register.html', {'user_form': use_form} )
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('/cars/cars_list')
+    elif request.method == "GET":
+        use_form = UserCreationForm()
+    return render(request, 'register.html', {'user_form': user_form} )
