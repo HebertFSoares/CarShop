df3c9db406154ad7c3548db75540233ab02d3d4f
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-15 13:20:16 -0300

    Atualizando as views

--- cars/views.py
+++ cars/views.py
@@ -5,33 +5,6 @@ from .forms import CarModelForm
 from django.views import View
 from django.views.generic import ListView
 
-
-'''
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
-    
-    if search:
-        cars = Car.objects.filter(model__icontains=search)
-    
-    return render (
-        request, 'cars.html',
-        {'cars': cars }
-    )
-'''
-class CarsView(View):
-    
-    def get(self,request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
-    
-        if search:
-            cars = Car.objects.filter(model__icontains=search)
-    
-        return render (
-            request, 'cars.html',
-            {'cars': cars }
-     )
 class CarsListView(ListView):
     model = Car
     template_name = 'cars.html'
@@ -45,19 +18,6 @@ class CarsListView(ListView):
             cars = Car.objects.filter(model__icontains=search)
         return cars
     
-    
-'''      
-def new_car(request):
-    if request.method == "POST":
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect(reverse('cars_list'))
-        
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_cars.html',{'new_car_form': new_car_form})
-'''
         
 class NewCar(View):
     
