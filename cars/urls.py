61a90ba2b22c07b5e2619bb47c0e35cd1a57f745
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-17 14:34:56 -0300

    Criação da Url de CarUpdateView

--- cars/urls.py
+++ cars/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
 from . import views
+
 urlpatterns = [
     path('cars_list/', views.CarsListView.as_view() , name="cars_list"),
     path('new_car/', views.NewCarCreateView.as_view(), name="new_car"),
-    path('car/<int:pk>/', views.CarDetailView.as_view(), name="car_detail")
+    path('car/<int:pk>/', views.CarDetailView.as_view(), name="car_detail"),
+    path('car/<int:pk>/update/', views.CarUpdateView.as_view(), name="car_update"),
 ]
