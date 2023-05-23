e884bd7975cbad68c3d6917aef5c64f7f588c7ef
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-23 19:04:05 -0300

    Adicionando autorização nas views

--- cars/views.py
+++ cars/views.py
@@ -5,6 +5,9 @@ from .forms import CarModelForm
 from django.views import View
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 class CarsListView(ListView):
     model = Car
@@ -19,16 +22,18 @@ class CarsListView(ListView):
             cars = Car.objects.filter(model__icontains=search)
         return cars
     
+@method_decorator(login_required, name="dispatch")
 class NewCarCreateView(CreateView):
     model = 'car'
     form_class = CarModelForm
     template_name = 'new_cars.html'
     success_url = reverse_lazy('cars_list')
-    
+
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
-    
+
+@method_decorator(login_required, name="dispatch")    
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -36,7 +41,8 @@ class CarUpdateView(UpdateView):
     
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
-    
+
+@method_decorator(login_required, name="dispatch")
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
