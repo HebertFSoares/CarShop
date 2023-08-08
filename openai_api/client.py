51c9a63b5b484c22d4cee02d6c6b77937fd64bb4
Hebert Soares <hebertsoaresof@gmail.com>
2023-08-07 23:43:38 -0300

    Configurando a api da openia

--- openai_api/client.py
+++ openai_api/client.py
@@ -4,7 +4,7 @@ def get_car_ai_bio(model, brand, year):
     prompt = ''''
                 Me mostre uma descrição de venda para o carro {} {} {} em apenas 200 caracteres. Descreva especifícações técnicas desse modelo de carro.
              '''
-    openai.api_key = "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"
+    openai.api_key = "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"
     prompt = prompt.format(brand,model,year)
     response = openai.Completion.create(
         model="text-davinci-003",
