7b534048c990a3cf6f2a5d776d032524da59034c
Hebert Soares <hebertsoaresof@gmail.com>
2023-08-19 16:06:36 -0300

    Update client.py

--- openai_api/client.py
+++ openai_api/client.py
@@ -4,7 +4,7 @@ def get_car_ai_bio(model, brand, year):
     prompt = ''''
                 Me mostre uma descrição de venda para o carro {} {} {} em apenas 200 caracteres. Descreva especifícações técnicas desse modelo de carro.
              '''
-    openai.api_key = "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"
+    openai.api_key = "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"
     prompt = prompt.format(brand,model,year)
     response = openai.Completion.create(
         model="text-davinci-003",
